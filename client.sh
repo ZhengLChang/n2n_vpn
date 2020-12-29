67b08a572f8301380e6e325808158258ace8c9a2
zheng <zhengtianjie@me.com>
2020-12-29 09:18:56 +0000

    debug

---
+++ client.sh
@@ -0,0 +1,1 @@
+edge -a 10.10.10.11 -c mynetwork -k Zzshanguangy -l ${serverIp}:56560
---
+++ init_env.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+yum -y install epel-release
+yum -y install python3
+yum -y install gcc
+yum -y install gcc-c++
+yum -y install python3-devel
+yum -y install lrzsz
+yum -y install vim
+yum -y install google-chrome-stable
+yum -y install net-tools
+yum -y install nscd
+yum -y install nload
+yum -y install libxml2-devel
+yum -y install autoconf
+yum -y install libtool
+pip3 install gevent
+pip3 install lxml
+pip3 install requests
+pip3 install redis
+pip3 install brotli
+pip3 install youtube-dl
+pip3 install cython
+pip3 install urllib3
+pip3 install selenium
+pip3 install webdriver-manager
+pip3 install browsermob-proxy
+pip3 install pillow
+pip3 install Image
+pip3 install paramiko
+pip3 install numpy
+pip3 install google
+pip3 install libxml2-python3
+pip3 install --upgrade google-api-python-client
+
+
