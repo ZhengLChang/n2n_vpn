3c0c11713e4fdc70b3d9330563400c5799276ce0
zheng <zhengtianjie@me.com>
2021-01-06 12:21:05 +0000

    modify iptables.sh

--- iptables.sh
+++ iptables.sh
@@ -1,5 +1,6 @@
 edgeIP=$( ip addr | egrep -o '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | egrep "^10\." | head -n 1 )
 publicIP=$( ip addr | egrep -o '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | egrep -v "^192\.168|^172\.1[6-9]\.|^172\.2[0-9]\.|^172\.3[0-2]\.|^10\.|^127\.|^255\.|^0\." | head -n 1 )
+echo 1 > /proc/sys/net/ipv4/ip_forward
 iptables -F
 iptables -F -t nat
 iptables -I INPUT -p udp --dport 4500 -j ACCEPT   
